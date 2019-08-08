834388227da8fceafa44e0779f3caaf10b2b67f1
Simon Laux <mobile.info@simonlaux.de>
2019-08-08 16:52:06 +0200

    update & add todo comments

--- init.sh
+++ init.sh
@@ -3,4 +3,11 @@ set -e
 
 git submodule update --remote
 
+# the following 2 points are covered by the node branch for now
+# idea replace all "release" with "debug" in deltachat-node/binding.gyp
+# idea remove --release argument in deltachat-node/scripts/rebuild-core.js
+# idea do the change in package.json in deltachat-node/
+
+# todo deltachat-desktop package json dc-node version to "../deltachat-node"
+
 source ./scripts/install.sh
